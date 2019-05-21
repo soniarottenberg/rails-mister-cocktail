dffbf292a8b08b3dd24cf7573056a3ba61e44b99
Sonia Rottenberg <sonia.rottenberg@gmail.com>
2019-05-21 17:30:16 +0200

    logic done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+puts 'Generating ingredients...please wait!'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+serialized_ingredients = open(url).read
+
+ingredients = JSON.parse(serialized_ingredients)
+list = ingredients["drinks"]
+
+list.each do |element|
+  element.each do |key, value|
+    # puts value
+    Ingredient.create(name: value)
+  end
+end
+
+puts 'Done <3'
